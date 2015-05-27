19b9542e8c54dbb77db984ee1f4094ad03597e20
Michael Crump <mbcrump29@gmail.com>
2015-05-26 20:21:05 -0500

    Initiail Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var Greeter = (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+})();
+var greeter = new Greeter(" Twitter");
+//# sourceMappingURL=main.js.map
